50cd24c0745fce76d48e9b92e1f6b731055ab6a4
Ko Myoungmin <miung222@gmail.com>
2020-03-08 15:54:01 +0900

    시간설정, 알람설정, 노래설정 모드 선택

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,7 +20,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include <stdio.h>
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -33,6 +33,53 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define UP_KEY  65
+#define DOWN_KEY 66
+#define RIGHT_KEY 67
+#define LEFT_KEY  68
+#define SEL_KEY  13
+
+#define LONG_CLICK_MIN 20
+#define LONG_CLICK_MAX 50
+#define LONG_CLICK_COUNT 30
+
+#define DOUBLE_CLICK_MIN 100
+#define DOUBLE_CLICK_MAX 200
+
+#define NORMAL_CLICK_MIN 500
+
+enum CLOCK_MODE{
+	NORMAL_STATE,
+	TIME_SETTING,
+	ALARM_TIME_SETTING,
+	MUSIC_SELECT
+};
+
+enum CLOCK_BUTTON{
+	NO_KEY,
+	UP,
+	DOWN,
+	RIGHT,
+	LEFT,
+	SEL
+};
+
+//설정모드 자리 나태니는 열거형
+enum SETTING{
+	NOT,
+	AP,
+	SET_H,
+	SET_M,
+	SET_S
+};
+struct clock_state{
+	enum CLOCK_MODE mode;
+	enum CLOCK_BUTTON button;
+};
+
+struct clock_state current_state;
+enum SETTING setting = AP;
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -58,121 +105,581 @@ static void MX_USART3_UART_Init(void);
 static void MX_RTC_Init(void);
 static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
+void time_display(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
 volatile int second, minute, hour;
 volatile int AL_second, AL_minute, AL_hour;
 volatile int timer_count;
-volatile int meridiem;	//AM, PM 구분하는 변수
-volatile int AL_meridiem;
+char line[2][18], time_str[16];
+uint8_t key_value;
 char uart_buf[40];
 
 char rcv_byte;
-uint32_t last_time,current_time,time_interval;
-volatile int long_key;
-
-enum CLOCK_MODE{
-   NORMAL_STATE,
-   TIME_SETTING,
-   ALARM_TIME_SETTING,
-   MUSIC_SELECT
-};
-
-enum CLOCK_MODE clock_mode;
-
-
-void time_display(void);
+uint32_t last_time,current_time;
+uint32_t time_interval;
+uint32_t long_key_count;
+uint8_t key_value;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-	if(htim->Instance == TIM2)	//타이머2 인터럽트
-	{
-		if(timer_count % 100 == 0)	//1초마다
-		{
-			hour = timer_count / 360000;	//시
-			minute = (timer_count % 360000) / 6000;	//분
-			second = ((timer_count % 360000) % 6000) / 100;	//초
+  if(htim->Instance==TIM2)
+  {
+    if((timer_count%100)==0)
+    {
+      second++;
 
-			time_display();	//시간 출력 함수
-		}
-		timer_count++;
+      if(second>=60)
+      {
+        minute++;
+        second = 0;
+      }
+      else if(second < 0) second = 0;
+      if(minute>=60)
+      {
+        hour++;
+        minute = 0;
+      }
+      else if(minute < 0) minute = 0;
+      if(hour>=24 || hour < 0)
+      {
+        hour = 0;
+      }
 
-		if(timer_count == 4320000)	//12시간이 될때
-		{
-			timer_count = 0;	//카운트 초기화
-			meridiem = !meridiem;	//AM, PM toggle
-		}
-	}
+      time_display();
+    }
+	timer_count++;
+  }
 }
 
 void time_display(void)
 {
-	memset(uart_buf, 0, 40);	//문자열 0으로 초기화
+	switch(current_state.mode)
+	{
+	case NORMAL_STATE:
+		memset(line,0,sizeof(line));
+		sprintf(line[0],"Korea Polytech  \r\n");
+		if(hour>=12)
+		{
+		  strncpy(line[1],"PM ",3);
+		  sprintf(time_str,"%02d:%02d:%02d\r\n",hour-12,minute,second);
+		}
+		else
+		{
+		  strncpy(line[1],"AM ",3);
+		  sprintf(time_str,"%02d:%02d:%02d\r\n",hour,minute,second);
+		}
+		strcat(line[1],time_str);
+		break;
+	case TIME_SETTING:
+		memset(line,0,sizeof(line));
+		sprintf(line[0],"Korea Polytech  \r\n");
+		//설정모드 자리가 어디 선택 되어 있는 가에 따라 깜빡임 다른 모습으로 출력
+		switch(setting)
+		{
+		case AP:
+			if(timer_count % 200 == 0)
+			{
+				if(hour>=12)
+				{
+				  strncpy(line[1],"   ",3);
+				  sprintf(time_str,"%02d:%02d:%02d\r\n",hour-12,minute,second);
+				}
+				else
+				{
+				  strncpy(line[1],"   ",3);
+				  sprintf(time_str,"%02d:%02d:%02d\r\n",hour,minute,second);
+				}
+				strcat(line[1],time_str);
 
-	if(clock_mode == NORMAL_STATE)
-	{
-		//AM일때 출력
-		if(meridiem == 0) sprintf(uart_buf, "Korea Polytech  \r\nAM %02d:%02d:%02d   \r\n",hour, minute, second);
-		//PM일때 출력
-		else sprintf(uart_buf, "Korea Polytech  \r\nPM %02d:%02d:%02d   \r\n",hour, minute, second);
-	}
-	else if(clock_mode == TIME_SETTING)
-	{
-		//깜빡임 표시
-		if(second % 2 == 0) sprintf(uart_buf, "Korea Polytech  \r\n   %02d:%02d:%02d   \r\n",hour, minute, second);
-		else
-		{
-			//AM일때 출력
-			if(meridiem == 0) sprintf(uart_buf, "Korea Polytech  \r\nAM %02d:%02d:%02d   \r\n",hour, minute, second);
-			//PM일때 출력
-			else sprintf(uart_buf, "Korea Polytech  \r\nPM %02d:%02d:%02d   \r\n",hour, minute, second);
+			}
+			else
+			{
+				if(hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d\r\n",hour-12,minute,second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d\r\n",hour,minute,second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			break;
+		case SET_H:
+			if(timer_count % 200 == 0)
+			{
+				if(hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"  :%02d:%02d\r\n",minute,second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"  :%02d:%02d\r\n",minute,second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			else
+			{
+				if(hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d\r\n",hour-12,minute,second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d\r\n",hour,minute,second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			break;
+		case SET_M:
+			if(timer_count % 200 == 0)
+			{
+				if(hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:  :%02d\r\n",hour-12,second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:  :%02d\r\n",hour,second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			else
+			{
+				if(hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d\r\n",hour-12,minute,second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d\r\n",hour,minute,second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			break;
+		case SET_S:
+			if(timer_count % 200 == 0)
+			{
+				if(hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:%02d:  \r\n",hour-12,minute);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:%02d:  \r\n",hour,minute);
+				}
+				strcat(line[1],time_str);
+
+			}
+			else
+			{
+				if(hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d\r\n",hour-12,minute,second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d\r\n",hour,minute,second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			break;
+		default:
+			break;
 		}
-	}
-	else if(clock_mode == ALARM_TIME_SETTING)
-	{
-		//깜빡임 표시
-		if(second % 2 == 0) sprintf(uart_buf, "Korea Polytech  \r\n   %02d:%02d:%02d AL\r\n",AL_hour, AL_minute, AL_second);
-		else
+		break;
+	case ALARM_TIME_SETTING:
+		memset(line,0,sizeof(line));
+		sprintf(line[0],"Korea Polytech  \r\n");
+		//설정모드 자리가 어디 선택 되어 있는 가에 따라 깜빡임 다른 모습으로 출력
+		switch(setting)
 		{
-			//AM일때 출력
-			if(AL_meridiem == 0) sprintf(uart_buf, "Korea Polytech  \r\nAM %02d:%02d:%02d AL\r\n",AL_hour, AL_minute, AL_second);
-			//PM일때 출력
-			else sprintf(uart_buf, "Korea Polytech  \r\nPM %02d:%02d:%02d AL\r\n",AL_hour, AL_minute, AL_second);
+		case AP:
+			if(timer_count % 200 == 0)
+			{
+				if(AL_hour>=12)
+				{
+				  strncpy(line[1],"   ",3);
+				  sprintf(time_str,"%02d:%02d:%02d AL\r\n",AL_hour-12,AL_minute,AL_second);
+				}
+				else
+				{
+				  strncpy(line[1],"   ",3);
+				  sprintf(time_str,"%02d:%02d:%02d AL\r\n",AL_hour,AL_minute,AL_second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			else
+			{
+				if(AL_hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d AL\r\n",AL_hour-12,AL_minute,AL_second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d AL\r\n",AL_hour,AL_minute,AL_second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			break;
+		case SET_H:
+			if(timer_count % 200 == 0)
+			{
+				if(AL_hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"  :%02d:%02d AL\r\n",AL_minute,AL_second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"  :%02d:%02d AL\r\n",AL_minute,AL_second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			else
+			{
+				if(AL_hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d AL\r\n",AL_hour-12,AL_minute,AL_second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d AL\r\n",AL_hour,AL_minute,AL_second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			break;
+		case SET_M:
+			if(timer_count % 200 == 0)
+			{
+				if(AL_hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:  :%02d AL\r\n",AL_hour-12,AL_second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:  :%02d AL\r\n",AL_hour,AL_second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			else
+			{
+				if(AL_hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d AL\r\n",AL_hour-12,AL_minute,AL_second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d AL\r\n",AL_hour,AL_minute,AL_second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			break;
+		case SET_S:
+			if(timer_count % 200 == 0)
+			{
+				if(AL_hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:%02d:   AL\r\n",AL_hour-12,AL_minute);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:%02d:   AL\r\n",AL_hour,AL_minute);
+				}
+				strcat(line[1],time_str);
+
+			}
+			else
+			{
+				if(AL_hour>=12)
+				{
+				  strncpy(line[1],"PM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d AL\r\n",AL_hour-12,AL_minute,AL_second);
+				}
+				else
+				{
+				  strncpy(line[1],"AM ",3);
+				  sprintf(time_str,"%02d:%02d:%02d AL\r\n",AL_hour,AL_minute,AL_second);
+				}
+				strcat(line[1],time_str);
+
+			}
+			break;
+		default:
+			break;
 		}
+		break;
+	case MUSIC_SELECT:
+		memset(line,0,sizeof(line));
+		sprintf(line[0],"Three Bears     \r\n");
+		sprintf(line[1],"Spring Water    \r\n");
+		break;
+	default:
+		break;
 	}
-	else if(clock_mode == MUSIC_SELECT)
-	{
-		sprintf(uart_buf, "Three Bears     \r\nSpring Water    \r\n");
-	}
-	HAL_UART_Transmit_IT(&huart3, uart_buf, sizeof(uart_buf));	//화면 출력
+
+
+  HAL_UART_Transmit_IT(&huart3,(uint8_t *)line,sizeof(line));
 }
-
-/* Arrow Keys mapping
- * UP = 'A' = 65
- * DOWN = 'B' = 66
- * RIGHT = 'C' = 67
- * LEFT = 'D' = 68
- * ENTER = 13
- */
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
   if (huart->Instance == USART3)
   {
-	/* Transmit one byte with 100 ms timeout */
-	//HAL_UART_Transmit(&huart3, &rcv_byte, 1, 100);
 
 	current_time = HAL_GetTick();
     time_interval = current_time - last_time;
     last_time = current_time;
 
+    switch(current_state.mode)
+    {
+      case NORMAL_STATE:
+    	  switch(key_value)
+    	  {
+    	  	  case SEL_KEY:
+        		  current_state.button = SEL;
+        		  mode_analysis();
+    	  		  break;
+    	  	  default:  // Arrow keys is Not valid in NORMAL state
+    	  		  break;
+    	  }
+    	  break;
+      case TIME_SETTING:
+    	  switch(key_value)
+    	  {
+    	  	  case SEL_KEY:
+    	  		  if(time_interval>= NORMAL_CLICK_MIN)
+    	  		  {
+    	  			  setting = AP;
+    	  			  current_state.mode = NORMAL_STATE;
+    	  		  }
+    	  		  //더블클릭하면 처음 클릭 부분에서 타임세팅으로 넘어가기 때문에 여기서 음악선택모드를 판별
+    	  		  //mode_analysis()에서 판별하면 타임세팅에서 더블클릭해야 음악선택모드로 넘어감
+    	  		  else if(time_interval>=DOUBLE_CLICK_MIN && time_interval <=DOUBLE_CLICK_MAX)
+    	  		  {
+    	  			  current_state.mode = MUSIC_SELECT;
+    	  			  long_key_count = 0;
+
+    	  			  memset(uart_buf,0,sizeof(uart_buf));
+    	  			  sprintf(uart_buf,"MUSIC_SELECT %d\r\n",time_interval);
+    	  			  HAL_UART_Transmit_IT(&huart3,uart_buf,sizeof(uart_buf));
+
+    	  	    }
+    	  		  break;
+    	  	  case UP_KEY:
+    	  		  switch(setting)
+    	  		  {
+    	  		  case AP:
+    	  			  hour += 12;
+    	  			  break;
+    	  		  case SET_H:
+    	  			  hour++;
+    	  			  break;
+    	  		  case SET_M:
+    	  			  minute++;
+    	  			  break;
+    	  		  case SET_S:
+    	  			  second++;
+    	  			  break;
+    	  		  default:
+    	  			  break;
+    	  		  }
+    	  		  break;
+    	  	  case DOWN_KEY:
+    	  		  switch(setting)
+    	  		  {
+    	  		  case AP:
+    	  			  hour -= 12;
+    	  			  break;
+    	  		  case SET_H:
+    	  			  hour--;
+    	  			  break;
+    	  		  case SET_M:
+    	  			  minute--;
+    	  			  break;
+    	  		  case SET_S:
+    	  			  second--;
+    	  			  break;
+    	  		  default:
+    	  			  break;
+    	  		  }
+    	  		  break;
+    	  	  case RIGHT_KEY:
+    	  		  setting++;
+    	  		  if(setting == 5) setting = AP;
+    	  		  break;
+    	  	  case LEFT_KEY:
+    	  		  setting--;
+    	  		  if(setting == NOT) setting = SET_S;
+    	  		  break;
+    	  	  default:  // Arrow keys is Not valid in NORMAL state
+    	  		  break;
+    	  }
+    	  break;
+      case ALARM_TIME_SETTING:
+    	  switch(key_value)
+    	  {
+    	  	  case SEL_KEY:
+    	  		  if(time_interval>= NORMAL_CLICK_MIN)
+    	  		  {
+    	  			  setting = AP;
+    	  			  current_state.mode = NORMAL_STATE;
+    	  		  }
+    	  		  break;
+    	  	  case UP_KEY:
+    	  		  switch(setting)
+    	  		  {
+    	  		  case AP:
+    	  			  AL_hour += 12;
+    	  			  if(AL_hour > 12 || AL_hour <= 0) AL_hour = 0;
+    	  			  break;
+    	  		  case SET_H:
+    	  			  AL_hour++;
+    	  			  if(AL_hour > 12 || AL_hour <= 0) AL_hour = 0;
+    	  			  break;
+    	  		  case SET_M:
+    	  			  AL_minute++;
+    	  			  if(AL_minute > 60 || AL_minute <= 0) AL_minute = 0;
+    	  			  break;
+    	  		  case SET_S:
+    	  			  AL_second++;
+    	  			  if(AL_second > 60 || AL_second <= 0) AL_second = 0;
+    	  			  break;
+    	  		  default:
+    	  			  break;
+    	  		  }
+    	  		  break;
+    	  	  case DOWN_KEY:
+    	  		  switch(setting)
+    	  		  {
+    	  		  case AP:
+    	  			  AL_hour -= 12;
+    	  			  if(AL_hour > 12 || AL_hour <= 0) AL_hour = 0;
+    	  			  break;
+    	  		  case SET_H:
+    	  			  AL_hour--;
+    	  			  if(AL_hour > 12 || AL_hour <= 0) AL_hour = 0;
+    	  			  break;
+    	  		  case SET_M:
+    	  			  AL_minute--;
+    	  			  if(AL_minute > 60 || AL_minute <= 0) AL_minute = 0;
+    	  			  break;
+    	  		  case SET_S:
+    	  			  AL_second--;
+    	  			  if(AL_second > 60 || AL_second <= 0) AL_second = 0;
+    	  			  break;
+    	  		  default:
+    	  			  break;
+    	  		  }
+    	  		  break;
+    	  	  case RIGHT_KEY:
+    	  		  setting++;
+    	  		  if(setting == 5) setting = AP;
+    	  		  break;
+    	  	  case LEFT_KEY:
+    	  		  setting--;
+    	  		  if(setting == NOT) setting = SET_S;
+    	  		  break;
+    	  	  default:  // Arrow keys is Not valid in NORMAL state
+    	  		  break;
+    	  }
+    	  break;
+      case MUSIC_SELECT:
+    	  switch(key_value)
+    	  {
+	  	  	  case SEL_KEY:
+	  	  		  if(time_interval>= NORMAL_CLICK_MIN) current_state.mode = NORMAL_STATE;
+	  	  		  break;
+	  	  	  case UP_KEY:
+	  	  		  memset(uart_buf,0,sizeof(uart_buf));
+	  	  		  sprintf(uart_buf,"Three Bears selected\r\n");
+	  	  		  HAL_UART_Transmit_IT(&huart3,uart_buf,sizeof(uart_buf));
+	  	  		  break;
+	  	  	  case DOWN_KEY:
+	  	  		  memset(uart_buf,0,sizeof(uart_buf));
+	  	  		  sprintf(uart_buf,"Spring Water selected\r\n");
+	  	  		  HAL_UART_Transmit_IT(&huart3,uart_buf,sizeof(uart_buf));
+	  	  		  break;
+    	  }
+    	  break;
+      default:
+    	  break;
+    }
+
+
 	/* Receive one byte in interrupt mode */
-	HAL_UART_Receive_IT(&huart3, &rcv_byte, 1);	//수신 인터럽트 진행 후 다시 이렇게 설정 해줘야 다시 수신 인터럽트 가능??
+	HAL_UART_Receive_IT(&huart3, &key_value, 1);
   }
 }
-/* USER CODE END PFP */
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
+//타임세팅, 알람세팅으로 넘어가는 여부 판별 함수 (음악선택은 제외)
+void mode_analysis(void)
+{
+    if(time_interval>=LONG_CLICK_MIN && time_interval <=LONG_CLICK_MAX)
+    {
+    	long_key_count++;
+    }
+    else if(time_interval>= NORMAL_CLICK_MIN)
+    {
+      current_state.mode = TIME_SETTING;
+      long_key_count = 0;
 
+      memset(uart_buf,0,sizeof(uart_buf));
+      sprintf(uart_buf,"TIME_SETTING  %d\r\n",time_interval);
+      HAL_UART_Transmit_IT(&huart3,uart_buf,sizeof(uart_buf));
+
+    }
+
+    if(long_key_count>=LONG_CLICK_COUNT)
+    {
+      current_state.mode  = ALARM_TIME_SETTING;
+      long_key_count = 0;
+
+      memset(uart_buf,0,sizeof(uart_buf));
+      sprintf(uart_buf,"ALARM_TIME_SETTING  %d\r\n",time_interval);
+      HAL_UART_Transmit_IT(&huart3,uart_buf,sizeof(uart_buf));
+    }
+}
 /* USER CODE END 0 */
 
 /**
@@ -211,6 +718,9 @@ int main(void)
   HAL_TIM_Base_Start_IT(&htim2);
 
   HAL_UART_Receive_IT(&huart3,&rcv_byte,1);	//UART 수신 인터럽트 초기설정 이거 없으면 인터럽트 시작 안함
+  
+  current_state.mode = NORMAL_STATE;
+  current_state.button = NO_KEY;
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -218,37 +728,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	if(time_interval)
-	{
-		if(rcv_byte == 13 && time_interval >=20 && time_interval <= 50)	//길게누르면 카운트 올라감
-		{
-			long_key++;
-		}
 
-		if(rcv_byte == 13 && time_interval >=100 && time_interval <= 200)	//더블 클릭일때 모드 변경
-		{
-			if(clock_mode == TIME_SETTING) clock_mode = MUSIC_SELECT;
-		}
-
-		else if(rcv_byte == 13 && long_key == 30)	//길게 3초 누르면 모드 변경
-		{
-			long_key = 0;
-			if(clock_mode == TIME_SETTING) clock_mode = ALARM_TIME_SETTING;
-		}
-
-		else if(rcv_byte == 13 && time_interval > 1000)	//모드변경 버튼 누른 후 1초이상 지나야 적용 (앞에 모드변경들에 영향 없기 위해)
-		{
-			if(clock_mode == NORMAL_STATE) clock_mode = TIME_SETTING;
-			else clock_mode = NORMAL_STATE;
-		}
-
-		//클락 모드 확인용 UART 송신
-		memset(uart_buf,0,sizeof(uart_buf));
-		sprintf(uart_buf,"%d\r\n",clock_mode);
-		HAL_UART_Transmit_IT(&huart3,uart_buf,sizeof(uart_buf));
-		//타임 인터벌 초기화
-		time_interval = 0;
-	}
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
